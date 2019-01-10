e7fb4380920403ba0e77a2325c1a8c9a73d5d767
vagrant <vagrant@homestead>
2019-01-10 03:49:21 +0000

    Finish login and logout

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Auth;
+
+class SessionsController extends Controller
+{
+
+
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+
+    // 用户登录认证判断
+    public function store(Request $request)
+    {
+        $credentials = $this->validate($request, [
+            'email'    => 'required|email|max:255',
+            'password' => 'required'
+        ]);
+
+
+        // 登录判断
+        if (Auth::attempt($credentials, $request->has('remember'))) {
+            session()->flash('success', '欢迎回来！');
+            return redirect()->route('users.show', [Auth::user()]);
+        } else {
+            session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
+            return redirect()->back();
+        }
+
+        return;
+    }
+
+
+    // 退出
+    public function destroy()
+    {
+        Auth::logout();
+        session()->flash('success', '您已成功退出！');
+        return redirect('login');
+    }
+
+}

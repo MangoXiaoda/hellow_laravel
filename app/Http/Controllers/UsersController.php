b81712f757b8a623db7c2435d010bd7819c335db
vagrant <vagrant@homestead>
2019-01-14 02:26:47 +0000

    Finish user edit, update, index, and destroy actions

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -11,6 +11,24 @@ class UsersController extends Controller
 {
 
 
+    public function __construct()
+    {
+        $this->middleware('auth', [
+            'except' => ['show', 'create', 'store', 'index']
+        ]);
+
+        $this->middleware('guest',[
+            'only' => ['create']
+        ]);
+    }
+
+
+    // 用户列表
+    public function index()
+    {
+        $users = User::paginate(10);
+        return view('users.index', compact('users'));
+    }
 
     public function create()
     {
@@ -52,4 +70,38 @@ class UsersController extends Controller
         return view('users.edit', compact('user'));
     }
 
+    // 更新
+    public function update(User $user, Request $request)
+    {
+        $this->validate($request, [
+            'name'     => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6'
+        ]);
+
+        $this->authorize('update', $user);
+
+        $data = [];
+        $data['name'] = $request->name;
+        if ($request->password) {
+            $data['password'] = bcrypt($request->password);
+        }
+
+        $user->update($data);
+
+        session()->flash('success', '个人资料更新成功!');
+
+        return redirect()->route('users.show', $user->id);
+    }
+
+    // 删除用户
+    public function destroy(User $user)
+    {
+        $this->authorize('destroy', $user);
+
+        $user->delete();
+
+        session()->flash('success', '成功删除用户！');
+
+        return back();
+    }
 }
